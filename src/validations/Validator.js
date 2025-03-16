a69ee51dbad393982639a35abb7b150862f3a9e2
Kinshuk2020 <kinshukgoyal2003@gmail.com>
2025-03-16 20:58:20 +0530

    add logging for request validation failures in Validator middleware

--- src/validations/Validator.js
+++ src/validations/Validator.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import { validationResult } from 'express-validator';
+import { logExecution } from '../service/log.service.js';
 
 // This is a middleware that validates the request body of the API endpoint
 export const validate = (validations) => {
@@ -16,6 +17,7 @@ export const validate = (validations) => {
             message: err.msg
         }));
 
+        logExecution("Request Validator", req.body, "", "Validation Failed", 0);
         res.status(StatusCodes.BAD_REQUEST).json({
             errors: formattedErrors,
             success: false,
